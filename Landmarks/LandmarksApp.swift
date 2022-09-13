514031c04cb7847f422e173e60c6a3509e8ccb67
zhunhe <xuzhunhe@gmail.com>
2022-09-13 12:57:55 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 허준혁 on 2022/09/13.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
