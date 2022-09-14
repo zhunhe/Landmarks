b1d171cd02758cf441135ce1427608e0938e087c
zhunhe <xuzhunhe@gmail.com>
2022-09-14 18:54:01 +0900

    SwiftUI Essentials

---
+++ Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by 허준혁 on 2022/09/14.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .environmentObject(ModelData())
+        }
     }
 }
