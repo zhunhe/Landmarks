b29a4ada17cc58e8dbf701c6ca5c71d7c1796a31
zhunhe <xuzhunhe@gmail.com>
2022-09-16 15:04:46 +0900

    Composing Complex Interfaces
    - Add a Category View
    - Create a Category List
    - Create a Category Row

---
+++ Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by 허준혁 on 2022/09/16.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
